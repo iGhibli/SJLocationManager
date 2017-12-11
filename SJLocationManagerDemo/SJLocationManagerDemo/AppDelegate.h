ba6a4b50890905c0db094abb38ae3dc10e329abb
iGhibli <iGhibli@163.com>
2017-12-11 16:39:47 +0800

    Empty project.

---
+++ SJLocationManagerDemo/SJLocationManagerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJLocationManagerDemo
+//
+//  Created by YF on 2017/12/11.
+//  Copyright © 2017年 iGhibli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SJLocationManagerDemo/SJLocationManagerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SJLocationManagerDemo
+//
+//  Created by YF on 2017/12/11.
+//  Copyright © 2017年 iGhibli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
